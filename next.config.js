eeaf68e3fff9ea41f8811f0326e8543a9ba63483
pkkwilliam <pkkwilliam919@gmail.com>
2024-03-25 16:41:35 -0700

    update config

--- next.config.js
+++ next.config.js
@@ -2,11 +2,6 @@
 const nextConfig = {
   reactStrictMode: false,
   output: "export",
-  images: {
-    loader: "akamai",
-    path: "",
-  },
-  assetPrefix: "./",
 };
 
 module.exports = nextConfig;
