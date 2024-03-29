f77e4d813871581a8b96bf56ec97fd080912fc62
pkkwilliam <pkkwilliam919@gmail.com>
2024-03-28 23:51:26 -0700

    add missing store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import { persistReducer } from 'redux-persist'
-import createWebStorage from 'redux-persist/lib/storage/createWebStorage';
-import searchSlice from './features/search-slice';
-import productSlice from './features/product-slice';
-import cartSlice from './features/cart-slice';
-import  wishlistSlice from './features/wishlist-slice';
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer } from "redux-persist";
+import createWebStorage from "redux-persist/lib/storage/createWebStorage";
+import searchSlice from "./features/search-slice";
+import productSlice from "./features/product-slice";
+import cartSlice from "./features/cart-slice";
+import languageSlice from "./features/language-slice";
+import wishlistSlice from "./features/wishlist-slice";
 
 const createNoopStorage = () => {
   return {
@@ -20,29 +21,32 @@ const createNoopStorage = () => {
   };
 };
 
-const storage = typeof window !== 'undefined' ? createWebStorage('local') : createNoopStorage();
-
+const storage =
+  typeof window !== "undefined"
+    ? createWebStorage("local")
+    : createNoopStorage();
 
 const persistConfig = {
-  key:'root',
+  key: "root",
   storage,
-  version:1,
-  blacklist:['search','products']
-}
-
+  version: 1,
+  blacklist: ["search", "products"],
+};
 
 const rootReducer = combineReducers({
-  search:searchSlice,
-  products:productSlice,
-  cart:cartSlice,
-  wishlist:wishlistSlice,
-})
+  search: searchSlice,
+  products: productSlice,
+  cart: cartSlice,
+  language: languageSlice,
+  wishlist: wishlistSlice,
+});
 
-const persistedReducer = persistReducer(persistConfig,rootReducer)
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
-  middleware:(getDefaultMiddleware) => getDefaultMiddleware({
-    serializableCheck:false
-  }),
-})
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
