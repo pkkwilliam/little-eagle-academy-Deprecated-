a144f3d70a777e3405028d884beadb682a63cda7
pkkwilliam <pkkwilliam919@gmail.com>
2024-05-21 22:21:12 -0700

    update payment successful page

--- src/components/payment/payment-course-enroll-table.jsx
+++ src/components/payment/payment-course-enroll-table.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Image } from "react-bootstrap";
+import { useSelector } from "react-redux";
 
 const PaymentenrollsTable = ({ enrolls = [] }) => {
+  const { selectedLanguage } = useSelector((state) => state.language);
   return (
     <div className="table-content table-responsive">
       <table className="table">
@@ -15,46 +17,55 @@ const PaymentenrollsTable = ({ enrolls = [] }) => {
           </tr>
         </thead>
         <tbody>
-          {(enrolls ?? []).map((item, index) => (
-            <tr key={index}>
-              <td className="product-thumbnail">
-                <Image
-                  src={item?.clazz.imageUrls[0]}
-                  style={{
-                    borderRadius: 8,
-                    width: "100%",
-                    height: "100%",
-                  }}
-                  alt="img not found"
-                />
-              </td>
-              <td className="product-name">
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    justifyContent: "center",
-                  }}
-                >
-                  {item.title}
-                  <div>
-                    <h6 style={{ color: "grey" }}>
-                      Student Name: {item.student.lastName}
-                    </h6>
-                    <h6 style={{ color: "grey" }}>DOB: MISSING!!!</h6>
+          {(enrolls ?? []).map((item, index) => {
+            const localizedClazz = getLocalize(selectedLanguage, item.clazz);
+            return (
+              <tr key={index}>
+                <td className="product-thumbnail">
+                  <Image
+                    src={item?.clazz.imageUrls[0]}
+                    style={{
+                      borderRadius: 8,
+                      width: "100%",
+                      height: "100%",
+                    }}
+                    alt="img not found"
+                  />
+                </td>
+                <td className="product-name">
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <strong
+                      dangerouslySetInnerHTML={{ __html: localizedClazz.name }}
+                    />
+                    <div>
+                      <h6 style={{ color: "grey" }}>
+                        Student Name:{" "}
+                        {`${item.student.lastName}, ${item.student.firstName}`}
+                      </h6>
+                    </div>
                   </div>
-                </div>
-              </td>
-              <td className="product-subtotal">
-                <span className="amount">${item.clazz.price}</span>
-              </td>
-            </tr>
-          ))}
+                </td>
+                <td className="product-subtotal">
+                  <span className="amount">${item.clazz.price}</span>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
   );
 };
+
+function getLocalize(language, localizedObject) {
+  return localizedObject?.localized?.[language];
+}
 
 export default PaymentenrollsTable;
